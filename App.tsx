e0d90f29dff505466c0af94550d3f46b48290055
UdhavBhagat <Udhav.Bhagat@edu.sait.ca>
2024-12-01 22:03:22 -0700

    Fixed issues with main app registration

---
+++ App.tsx
@@ -0,0 +1,15 @@
+// import { registerRootComponent } from "expo";
+import Main from "./src/Main.jsx"
+
+
+import React
+ from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+ function App() {
+    return(
+        <Main />
+    )
+ }
+
+ export default App
